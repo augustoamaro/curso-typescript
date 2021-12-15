ef20f110993d582154b2001150bdd0e0d333aba3
Augusto <augustoamaro@icloud.com>
2021-12-15 18:46:08 -0300

    Finished section 2

--- README.md
+++ README.md
@@ -2,13 +2,18 @@
 
 Seção 1: Introdução
 - Instalando TypeScript globalmente (sudo npm i -g typescript)
-- Iniciando typescript (tsc --init)
 - Instalando TypeScript/Node (sudo npm i -g ts-node)
 - Instalando Live Server (npm i -s live-server)
+- Iniciando typescript (tsc --init)
 - Monitorar mudanças no TypeScript (tsc -w)
 - Executar projeto (tsc -w / npm start)
 
+
 Seção 2: Usando Tipos
 
--
+- String, number, boolean, tipos explícitos, array, tuplas, enums, any, funções, objeto, alias, union types, never, null
+- Transformar código JS em TS
+
+
+Seção 3: Entendendo o Compilador TypeScript
 
--- tipos/tipos.js
+++ tipos/tipos.js
@@ -48,3 +48,108 @@ carro = {
     ano: 2022
 };
 console.log(carro);
+// funções
+function retornaMeuNome() {
+    return nome;
+}
+console.log(retornaMeuNome());
+function digaOi() {
+    console.log('Oi');
+}
+digaOi();
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+console.log(multiplicar(2, 5));
+// tipo função
+let calculo;
+calculo = multiplicar;
+console.log(calculo(5, 6));
+// objetos
+let usuario = {
+    nome: 'João',
+    idade: 27
+};
+console.log(usuario);
+// Desafio 1
+let funcionario = {
+    supervisores: ['Ana', 'Fernando'],
+    baterPonto(horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário';
+        }
+    }
+};
+let funcionario2 = {
+    supervisores: ['Bia', 'Carlos'],
+    baterPonto(horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário';
+        }
+    }
+};
+console.log(funcionario.supervisores);
+console.log('Horário 8h: ', funcionario.baterPonto(8));
+console.log('Horario 9h: ', funcionario.baterPonto(5));
+// union types
+let nota = 10;
+console.log(`Minha nota é ${nota}`);
+// checando tipos
+let valor = 30;
+if (typeof valor === "number") {
+    console.log('É um valor number');
+}
+else {
+    console.log(typeof valor);
+}
+// never
+function falha(msg) {
+    throw new Error(msg);
+}
+const produto = {
+    nome: 'Sabão',
+    preco: 4,
+    validarProduto() {
+        if (!this.nome || this.nome.trim().length === 0) {
+            falha('Precisa ter um nome');
+        }
+        if (this.preco <= 0) {
+            falha('Preço inválido');
+        }
+    }
+};
+produto.validarProduto();
+// altura = null
+let altura = 12;
+let alturaOpcional = 12;
+alturaOpcional = null;
+const contato1 = {
+    nome: 'Fulano',
+    tel1: '9834789',
+    tel2: null
+};
+console.log(contato1.nome);
+console.log(contato1.tel1);
+console.log(contato1.tel2);
+let podeSerNulo = null; // any
+podeSerNulo = 12;
+console.log(podeSerNulo);
+let ContaBancaria = {
+    saldo: 3456,
+    depositar(valor) {
+        this.saldo += valor;
+    }
+};
+let correntista = {
+    nome: 'Jurandir',
+    contaBancaria: ContaBancaria,
+    contatos: ['3455454', '99898987']
+};
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);

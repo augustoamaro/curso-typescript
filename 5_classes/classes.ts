00d4c9586c02b4ec7be3891eaffd6796da8e103d
Augusto <augustoamaro@icloud.com>
2022-04-02 14:41:15 -0300

    Finished classes section

--- 5_classes/classes.ts
+++ 5_classes/classes.ts
@@ -77,3 +77,143 @@ class Cachorro {
 const cachorro1 = new Cachorro("Thor", "Boxer", "Branco");
 console.log(cachorro1);
 console.log(cachorro1.resumo());
+
+class Carro {
+  private velocidadeAtual: number = 0;
+
+  constructor(
+    public marca: string,
+    public modelo: string,
+    private velocidadeMaxima: number = 200
+  ) {}
+
+  protected alterarVelocidade(delta: number): number {
+    const novaVelocidade = this.velocidadeAtual + delta;
+    const velocidadeValida =
+      novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+
+    if (velocidadeValida) {
+      this.velocidadeAtual = novaVelocidade;
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+    }
+
+    return this.velocidadeAtual;
+  }
+
+  public acelerar(): number {
+    return this.alterarVelocidade(5);
+  }
+
+  public frear(): number {
+    return this.alterarVelocidade(-5);
+  }
+}
+
+const carroAdicionado = new Carro("Ferrari", "Enzo", 320);
+
+// ============ Herança ============== //
+class Ferrari extends Carro {
+  constructor(modelo: string, velocidadeMaxima: number) {
+    super("Ferrari", modelo, velocidadeMaxima);
+  }
+
+  public acelerar(): number {
+    return this.alterarVelocidade(30);
+  }
+
+  public frear(): number {
+    return this.alterarVelocidade(-40);
+  }
+}
+
+const f50 = new Ferrari("F50", 350);
+console.log(`${f50.marca} ${f50.modelo}`);
+console.log(f50.acelerar());
+console.log(f50.acelerar());
+console.log(f50.acelerar());
+console.log(f50.frear());
+
+// ======== Getters & Setters =========//
+class Pessoa {
+  private _idade: number = 0;
+
+  get idade(): number {
+    return this._idade;
+  }
+
+  set idade(valor: number) {
+    if (valor >= 0 && valor <= 120) {
+      this._idade = valor;
+    }
+  }
+}
+
+const pessoa1 = new Pessoa();
+pessoa1.idade = 10;
+console.log(pessoa1);
+
+// ======== ? =========//
+class Matematica {
+  static PI: number = 3.1416;
+
+  static areaCirc(raio: number): number {
+    return this.PI * raio * raio;
+  }
+}
+
+console.log(Matematica.areaCirc(4));
+
+// ======== Classe abstrada =========//
+abstract class Calculo {
+  protected resultado: number = 0;
+
+  abstract executar(...numeros: number[]): void;
+
+  getResultado(): number {
+    return this.resultado;
+  }
+}
+
+class Soma extends Calculo {
+  executar(...numeros: number[]): void {
+    this.resultado = numeros.reduce((t, a) => t + a);
+  }
+}
+
+class Multiplicacao extends Calculo {
+  executar(...numeros: number[]): void {
+    this.resultado = numeros.reduce((t, a) => t * a);
+  }
+}
+
+let c1 = new Soma();
+c1.executar(2, 3, 5);
+console.log(c1.getResultado());
+
+// ======== Singleton =========//
+class Unico {
+  private static instance: Unico = new Unico();
+  private constructor() {}
+
+  static getInstance(): Unico {
+    return Unico.instance;
+  }
+
+  agora() {
+    return new Date();
+  }
+}
+
+console.log(Unico.getInstance().agora());
+
+// ======== Readonly =========//
+class Aviao {
+  public readonly modelo: string;
+
+  constructor(modelo: string, public readonly prefixo: string) {
+    this.modelo = modelo;
+  }
+}
+const turboHelice = new Aviao("Tu-114", "PT-ABC");
+console.log(turboHelice);
--- build/5_classes/classes.js
+++ build/5_classes/classes.js
@@ -64,3 +64,115 @@ class Cachorro {
 const cachorro1 = new Cachorro("Thor", "Boxer", "Branco");
 console.log(cachorro1);
 console.log(cachorro1.resumo());
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carroAdicionado = new Carro("Ferrari", "Enzo", 320);
+// ============ Herança ============== //
+class Ferrari extends Carro {
+    constructor(modelo, velocidadeMaxima) {
+        super("Ferrari", modelo, velocidadeMaxima);
+    }
+    acelerar() {
+        return this.alterarVelocidade(30);
+    }
+    frear() {
+        return this.alterarVelocidade(-40);
+    }
+}
+const f50 = new Ferrari("F50", 350);
+console.log(`${f50.marca} ${f50.modelo}`);
+console.log(f50.acelerar());
+console.log(f50.acelerar());
+console.log(f50.acelerar());
+console.log(f50.frear());
+// ======== Getters & Setters =========//
+class Pessoa {
+    constructor() {
+        this._idade = 0;
+    }
+    get idade() {
+        return this._idade;
+    }
+    set idade(valor) {
+        if (valor >= 0 && valor <= 120) {
+            this._idade = valor;
+        }
+    }
+}
+const pessoa1 = new Pessoa();
+pessoa1.idade = 10;
+console.log(pessoa1);
+// ======== ? =========//
+class Matematica {
+    static areaCirc(raio) {
+        return this.PI * raio * raio;
+    }
+}
+Matematica.PI = 3.1416;
+console.log(Matematica.areaCirc(4));
+// ======== Classe abstrada =========//
+class Calculo {
+    constructor() {
+        this.resultado = 0;
+    }
+    getResultado() {
+        return this.resultado;
+    }
+}
+class Soma extends Calculo {
+    executar(...numeros) {
+        this.resultado = numeros.reduce((t, a) => t + a);
+    }
+}
+class Multiplicacao extends Calculo {
+    executar(...numeros) {
+        this.resultado = numeros.reduce((t, a) => t * a);
+    }
+}
+let c1 = new Soma();
+c1.executar(2, 3, 5);
+console.log(c1.getResultado());
+// ======== Singleton =========//
+class Unico {
+    constructor() { }
+    static getInstance() {
+        return Unico.instance;
+    }
+    agora() {
+        return new Date();
+    }
+}
+Unico.instance = new Unico();
+console.log(Unico.getInstance().agora());
+// ======== Readonly =========//
+class Aviao {
+    constructor(modelo, prefixo) {
+        this.prefixo = prefixo;
+        this.modelo = modelo;
+    }
+}
+const turboHelice = new Aviao("Tu-114", "PT-ABC");
+console.log(turboHelice);
